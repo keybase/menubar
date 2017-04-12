9e096f4c23459ff4887601f61443f91c219be50e
Steve Sanders <steve@keyba.se>
2017-04-12 13:47:23 -0700

    Windows positioner fix

--- example/main.js
+++ example/main.js
@@ -2,6 +2,10 @@ var menubar = require('menubar')
 
 var mb = menubar()
 
+if (process.platform === 'win32') {
+  mb.setOption('windowPosition', 'winStartMenu')
+}
+
 mb.on('ready', function ready () {
   console.log('app is ready')
 })
